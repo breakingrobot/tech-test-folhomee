de141fbdbf283c08d3ca27afdd5c551e78682453
Tony Briet <tony@unknownrobot.dev>
2020-07-30 02:08:50 +0200

    feat(header): header component base on grommet

---
+++ front/components/header.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { Header as GrommetHeader, Heading, Button, Box, ResponsiveContext } from 'grommet'
+
+export function Header() {
+  const size = React.useContext(ResponsiveContext);
+  return (
+    <GrommetHeader size="small" background="brand-secondary" pad={{horizontal: 'medium'}} gap="xxsmall" direction="row-responsive">
+      <Box align="center" direction="row-responsive" gap="medium" alignSelf={size !== 'small' ? 'start' : 'center'}>
+        <Heading level={4} alignSelf="center">
+          URL Shortener
+        </Heading>
+      </Box>
+      <Box align="center" direction="row-responsive" gap="medium">
+        <Link href="/">
+          <Button secondary label="GitHub" />
+        </Link>
+      </Box>
+    </GrommetHeader>
+  )
+}
+
+export default Header
